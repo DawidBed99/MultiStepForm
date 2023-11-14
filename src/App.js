f38146373a5f1d00e326ecbc40a9d2b2d0345e2c
Dawid Bednarz <160144@stud.prz.edu.pl>
2023-11-14 20:12:42 +0100

    basename for browserRouter

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename="/MultiStepForm">
         <div className="container">
           <Stepper />
           <Routes>
